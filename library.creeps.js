2634c459476b35522c2c2a8bfc4fec567cf0e3b3
David <davidpeatman@gmail.com>
2018-01-27 11:16:28 -0800

    Adding basic big harvester logic

--- library.creeps.js
+++ library.creeps.js
@@ -1,35 +1,37 @@
 const HARVESTER = 'harvester';
-const BIG_HARVESTER = HARVESTER;
 const UPGRADER = 'upgrader';
 const BUILDER = 'builder';
 
-const CREEP_PARTS = {
-    HARVESTER: [MOVE, CARRY, WORK],
-    BIG_HARVESTER: [MOVE, MOVE, CARRY, WORK]
+const BIG_CREEP_PARTS = {
+    [HARVESTER]: [MOVE, MOVE, MOVE, CARRY, WORK, WORK, WORK],
+    [UPGRADER]: [MOVE, CARRY, WORK],
+    [BUILDER]: [MOVE, CARRY, WORK]
 };
+
+const DEFUALT_PARTS = [MOVE, CARRY, WORK];
 
 module.exports = {
     HARVESTER,
     UPGRADER,
     BUILDER,
-    
+
     spawnCreep(spawn, creepType) {
-        switch (creepType) {
-        case HARVESTER:
-            if (spawn.spawnCreep([MOVE, CARRY, WORK], HARVESTER + Game.time, {memory: {role: HARVESTER, room: spawn.room.name}}) == OK) {
-                console.log('Spawning ' + HARVESTER);
-            }
-            break;
-        case UPGRADER:
-            if (spawn.spawnCreep([MOVE, CARRY, WORK], UPGRADER + Game.time, {memory: {role: UPGRADER, room: spawn.room.name}}) == OK) {
-                console.log('Spawning ' + UPGRADER);
-            }
-            break;
-        case BUILDER:
-            if (spawn.spawnCreep([MOVE, CARRY, WORK], BUILDER + Game.time, {memory: {role: BUILDER, room: spawn.room.name}}) == OK) {
-                console.log('Spawning ' + BUILDER);
-            }
-            break;
+        const creepParts = BIG_CREEP_PARTS[creepType] || DEFUALT_PARTS;
+        const code = spawn.spawnCreep(creepParts, creepType + Game.time, {
+                memory: {
+                    role: creepType,
+                    room: spawn.room.name
+                }
+            });
+        if (code === OK) {
+            console.log('Spawning ' + creepType);
+        } else if (code === ERR_NOT_ENOUGH_ENERGY && creepParts !== DEFUALT_PARTS) {
+            spawn.spawnCreep(DEFUALT_PARTS, creepType + Game.time, {
+                memory: {
+                    role: creepType,
+                    room: spawn.room.name
+                }
+            });
         }
     }
 };
