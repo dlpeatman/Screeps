f753892eb868c66257bb8c1e87d538d7a08aedb4
David <davidpeatman@gmail.com>
2018-01-30 07:36:59 -0800

    Fixing harvester edge case in Reaper

--- ai.reaper.js
+++ ai.reaper.js
@@ -1,16 +1,22 @@
+const creepsLibrary = require('library.creeps');
+
 module.exports = {
     clean() {
-        for (var name in Memory.creeps) {
+        for (const name in Memory.creeps) {
             if (!Game.creeps[name]) {
-                var creep = Memory.creeps[name];
-                var room = creep.room;
-                if (Memory.creeps[name].role === 'harvester') {
-                    var sourceId = Memory.creeps[name].sourceId;
-                    console.log('Removing ' + name + ' from source ' + sourceId);
-                    var enlistedCreeps = Memory.rooms[room].sources[sourceId];
-                    var index = enlistedCreeps.indexOf(name);
-                    if (index > -1) {
-                        Memory.rooms[room].sources[sourceId].splice(index, 1);
+                const creep = Memory.creeps[name];
+                const roomName = creep.room;
+                if (Memory.creeps[name].role === creepsLibrary.HARVESTER) {
+                    const sourceId = Memory.creeps[name].sourceId;
+                    if (sourceId) {
+                        console.log('Removing ' + name + ' from source ' + sourceId);
+                        const enlistedCreeps = Memory.rooms[roomName].sources[sourceId];
+                        const index = enlistedCreeps.indexOf(name);
+                        if (index > -1) {
+                            Memory.rooms[roomName].sources[sourceId].splice(index, 1);
+                        }
+                    } else {
+                        console.log('No sourceId found for ' + name + '. Deleting anyway.');
                     }
                 }
                 delete Memory.creeps[name];
